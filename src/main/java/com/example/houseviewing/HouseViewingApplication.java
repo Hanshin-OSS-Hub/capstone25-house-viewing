2abdb5ab2125046bcfccda5315460f2f4f58a9ca
Yoo <yooyoo9191@gmail.com>
2026-01-15 17:07:25 +0900

    feat: JPA 환경 셋팅 및 테스트

--- src/main/java/com/example/houseviewing/HouseViewingApplication.java
+++ src/main/java/com/example/houseviewing/HouseViewingApplication.java
@@ -1,7 +1,11 @@
 package com.example.houseviewing;
 
+import com.example.houseviewing.domain.Member;
+import com.example.houseviewing.repository.MemberRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HouseViewingApplication {
@@ -10,4 +14,15 @@ public class HouseViewingApplication {
 		SpringApplication.run(HouseViewingApplication.class, args);
 	}
 
+    @Bean
+    CommandLineRunner test(MemberRepository repo) {
+        return args -> {
+            Member m = new Member();
+            m.setName("테스트회원");
+            repo.save(m);
+        };
+    }
+
 }
+
+
