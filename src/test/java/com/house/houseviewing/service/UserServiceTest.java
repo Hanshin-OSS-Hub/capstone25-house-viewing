4f72349a51c5cf5cb8e440ff069da09256a6c9e6
Yoo <yooyoo9191@gmail.com>
2026-01-28 20:11:52 +0900

    feat: login test code add

--- src/test/java/com/house/houseviewing/service/UserServiceTest.java
+++ src/test/java/com/house/houseviewing/service/UserServiceTest.java
@@ -1,8 +1,10 @@
 package com.house.houseviewing.service;
 
+import com.house.houseviewing.dto.UserLoginRequest;
 import com.house.houseviewing.dto.UserRegisterRequest;
 import com.house.houseviewing.domain.User;
 import com.house.houseviewing.exception.DuplicateLoginIdException;
+import com.house.houseviewing.exception.LoginFailedException;
 import com.house.houseviewing.repository.UserRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
@@ -51,6 +54,37 @@ class UserServiceTest {
         Assertions.assertThatThrownBy(() -> userService.register(userDto))
                 .isInstanceOf(DuplicateLoginIdException.class);
     }
+
+    @Test
+    @DisplayName("로그인")
+    void 로그인_성공(){
+        // given
+        UserRegisterRequest userDto = getUserDto();
+        Long savedId = userService.register(userDto);
+
+        // when
+        UserLoginRequest user = new UserLoginRequest("yooyoo9191", "okok0635!");
+        Long login = userService.login(user);
+
+        // then
+        assertThat(login).isEqualTo(savedId);
+    }
+
+    @Test
+    @DisplayName("로그인 실패")
+    void 로그인_실패(){
+        UserRegisterRequest userDto = getUserDto();
+        Long savedId = userService.register(userDto);
+
+        // when
+        UserLoginRequest user = new UserLoginRequest("yooyoo9191", "okok0635");
+
+        // then
+        assertThatThrownBy(() -> userService.login(user))
+                .isInstanceOf(LoginFailedException.class);
+
+    }
+
 
     private UserRegisterRequest getUserDto() {
         UserRegisterRequest request = new UserRegisterRequest("유인근", "yooyoo9191@gmail.com", "yooyoo9191", "okok0635!");
