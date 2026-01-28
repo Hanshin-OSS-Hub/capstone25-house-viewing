3f9807c9e080f3cd2d44dbe787b83d4ff206a91d
Yoo <yooyoo9191@gmail.com>
2026-01-28 16:49:32 +0900

    feat: 회원가입 테스트 코드

--- src/main/java/com/house/houseviewing/api/dto/UserRegisterRequest.java
+++ src/main/java/com/house/houseviewing/api/dto/UserRegisterRequest.java
@@ -5,8 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
-@Getter
+@Getter @Setter
 @AllArgsConstructor
 public class UserRegisterRequest {
 
--- src/test/java/com/house/houseviewing/service/UserServiceTest.java
+++ src/test/java/com/house/houseviewing/service/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.house.houseviewing.service;
 
 import com.house.houseviewing.api.UserApiController;
+import com.house.houseviewing.api.dto.UserRegisterRequest;
 import com.house.houseviewing.domain.User;
 import com.house.houseviewing.repository.UserRepository;
 import org.assertj.core.api.Assertions;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,16 +21,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserServiceTest {
 
     @Autowired UserService userService;
-    @Autowired UserApiController userApiController;
+    @Autowired UserRepository userRepository;
 
     @Test
     @DisplayName("유저 회원등록")
-    void 회원가입(){
-    }
+    void 회원가입_성공(){
+        //given
+        UserRegisterRequest request = new UserRegisterRequest("유인근", "yooyoo9191@gmail.com", "yooyoo9191", "okok0635!");
+        Long userId = userService.save(request);
 
-    private static User getUser() {
-        return new User("유인근", "yooyoo9191@gmail.com", "yooyoo9191", "okok0635!");
-    }
+        //when
+        User user = userRepository.findById(userId).get();
 
+        //then
+        assertThat(user.getId()).isEqualTo(userId);
+        assertThat("유인근").isEqualTo(user.getName());
+        assertThat("yooyoo9191@gmail.com").isEqualTo(user.getEmail());
+        assertThat("yooyoo9191").isEqualTo(user.getLoginId());
+        assertThat("okok0635!").isEqualTo(user.getPassword());
+
+    }
 
 }
