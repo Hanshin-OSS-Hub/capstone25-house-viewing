c3141b421b98f30a5580ea3bcdcc352b78644b85
Yoo <yooyoo9191@gmail.com>
2026-02-03 21:06:38 +0900

    feat: 에러코드 수정

--- src/main/java/com/house/houseviewing/domain/global/exception/ExceptionCode.java
+++ src/main/java/com/house/houseviewing/domain/global/exception/ExceptionCode.java
@@ -11,7 +11,7 @@ public enum ExceptionCode {
     LOGIN_FAILED("VP001",HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호가 틀렸습니다."),
     FIND_LOGIN_ID_FAILED("VP002",HttpStatus.BAD_REQUEST, "이메일 또는 아이디가 틀렸습니다."),
     VERIFY_PASSWORD_FAILED("VP003", HttpStatus.BAD_REQUEST, "입력하신 정보가 틀렸습니다."),
-    MISMATCH_PASSWORD("VP003", HttpStatus.BAD_REQUEST, "입력하신 정보가 틀렸습니다.");
+    MISMATCH_PASSWORD("VP004", HttpStatus.BAD_REQUEST, "입력하신 정보가 틀렸습니다.");
 
 
 
