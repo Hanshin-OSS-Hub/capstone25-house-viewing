325662becf661ed35eed34df121513406ec513f1
Yoo <yooyoo9191@gmail.com>
2026-02-25 19:24:57 +0900

    feat: PdfUploadController

--- src/main/java/com/house/houseviewing/api/upload/controller/PdfUploadController.java
+++ src/main/java/com/house/houseviewing/api/upload/controller/PdfUploadController.java
@@ -1,7 +1,10 @@
 package com.house.houseviewing.api.upload.controller;
 
+import com.house.houseviewing.api.upload.service.PdfUploadService;
 import com.house.houseviewing.global.exception.AppException;
 import com.house.houseviewing.global.exception.ExceptionCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequiredArgsConstructor
 public class PdfUploadController {
+
+    private final PdfUploadService pdfUploadService;
 
     @PostMapping("/api/upload/pdf")
     public ResponseEntity<Void> upload(@RequestParam("file")MultipartFile file){
@@ -17,6 +23,8 @@ public class PdfUploadController {
             throw new AppException(ExceptionCode.VERIFY_FILE_FAILED);
         }
 
+        pdfUploadService.upload(file);
 
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- src/main/java/com/house/houseviewing/api/upload/service/PdfUploadService.java
+++ src/main/java/com/house/houseviewing/api/upload/service/PdfUploadService.java
@@ -1,7 +1,13 @@
 package com.house.houseviewing.api.upload.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class PdfUploadService {
+
+    public void upload(MultipartFile file){
+
+    }
+
 }
