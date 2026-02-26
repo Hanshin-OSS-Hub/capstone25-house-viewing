5df4de3c0a976de3f89acb804ef80ec01bccb888
mok <dnjswn0516@gmail.com>
2026-02-26 18:18:26 +0900

    feat: MyFragment UI 바인딩 및 레이아웃 설정

--- app/src/main/java/com/capstone/houseviewingapp/my/MyFragment.kt
+++ app/src/main/java/com/capstone/houseviewingapp/my/MyFragment.kt
@@ -1,7 +1,34 @@
 package com.capstone.houseviewingapp.my
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.capstone.houseviewingapp.R
+import com.capstone.houseviewingapp.databinding.FragmentHouseInfoStep2Binding
+import com.capstone.houseviewingapp.databinding.FragmentMyBinding
 
 class MyFragment : Fragment(R.layout.fragment_my) {
+    private var _binding: FragmentMyBinding? = null
+    private val binding get() = _binding!!
+    
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMyBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+    }
+    
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
