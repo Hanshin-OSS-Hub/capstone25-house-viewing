690cc982009edcdf3a53d3462f210376bb38b646
Yoo <yooyoo9191@gmail.com>
2026-03-04 16:08:24 +0900

    feat: JwtAuthenticationFilter

--- src/main/java/com/house/houseviewing/global/security/JwtAuthenticationFilter.java
+++ src/main/java/com/house/houseviewing/global/security/JwtAuthenticationFilter.java
@@ -1,4 +1,43 @@
 package com.house.houseviewing.global.security;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.util.Collections;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = resolveToken(request);
+
+        if(token != null && jwtTokenProvider.validateToken(token)){
+            String loginId = jwtTokenProvider.getLoginId(token);
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(loginId, null, Collections.emptyList());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
 }
