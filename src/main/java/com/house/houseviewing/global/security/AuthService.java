57417cfc9e1f8540a2bde2117667b46fba847373
Yoo <yooyoo9191@gmail.com>
2026-03-06 21:31:11 +0900

    Feat: AuthService

---
+++ src/main/java/com/house/houseviewing/global/security/AuthService.java
@@ -0,0 +1,26 @@
+package com.house.houseviewing.global.security;
+
+import com.house.houseviewing.domain.user.model.login.UserLoginRQ;
+import com.house.houseviewing.domain.user.model.login.UserLoginRS;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+
+    public UserLoginRS login(UserLoginRQ request) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getLoginId(),
+                        request.getPassword()
+                )
+        );
+        return new UserLoginRS("LOGIN_SUCCESS");
+    }
+}
