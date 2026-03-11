005b6330a800352922e9a122af74c061a05962ed
mok <dnjswn0516@gmail.com>
2026-03-11 18:04:19 +0900

    feat: AnalysisFragment 리팩토링 및 리사이클러뷰와 필터 기능 추가

--- app/src/main/java/com/capstone/houseviewingapp/analysis/AnalysisFrament.kt
+++
@@ -1,8 +0,0 @@
-package com.capstone.houseviewingapp.analysis
-
-import androidx.fragment.app.Fragment
-import com.capstone.houseviewingapp.R
-
-class AnalysisFrament : Fragment(R.layout.fragment_analysis) {
-
-}
