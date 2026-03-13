1f26d2d9ab18585dddd651d4736c86f8cdb56c33
Yoo <yooyoo9191@gmail.com>
2026-03-13 21:42:14 +0900

    refactor: UserMeResponse DTO

--- src/main/java/com/house/houseviewing/domain/user/dto/response/UserMeResponse.java
+++ src/main/java/com/house/houseviewing/domain/user/dto/response/UserMeResponse.java
@@ -1,13 +1,10 @@
 package com.house.houseviewing.domain.user.dto.response;
 
-import com.house.houseviewing.domain.house.dto.response.HouseMeResponse;
 import com.house.houseviewing.domain.subscription.dto.response.SubscriptionMeResponse;
 import com.house.houseviewing.domain.user.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.util.List;
 
 @AllArgsConstructor @Builder
 @Getter
@@ -15,20 +12,12 @@ public class UserMeResponse {
 
     private String name;
     private String email;
-    private String loginId;
-    private List<HouseMeResponse> houses;
     private SubscriptionMeResponse subscription;
 
     public static UserMeResponse from(UserEntity user){
         return UserMeResponse.builder()
                 .name(user.getName())
                 .email(user.getEmail())
-                .loginId(user.getLoginId())
-                .houses(
-                        user.getHouses().stream()
-                                .map(HouseMeResponse::from)
-                                .toList()
-                )
                 .subscription(SubscriptionMeResponse.from(user.getSubscription()))
                 .build();
     }
