347b5be929ad047f51267fcb7a5045566f501c55
Yoo <yooyoo9191@gmail.com>
2026-03-21 12:04:45 +0900

    feat: PreContractDiagnose API

--- src/main/java/com/house/houseviewing/domain/registrysnapshot/controller/RegistrySnapshotController.java
+++ src/main/java/com/house/houseviewing/domain/registrysnapshot/controller/RegistrySnapshotController.java
@@ -1,7 +1,7 @@
 package com.house.houseviewing.domain.registrysnapshot.controller;
 
 import com.house.houseviewing.application.registry.RegistryWorkflowService;
-import com.house.houseviewing.domain.registrysnapshot.service.RegistrySnapshotService;
+import com.house.houseviewing.domain.registrysnapshot.dto.request.PreContractDiagnosisRequest;
 import com.house.houseviewing.global.file.pdf.dto.PdfDownloadResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +21,15 @@ public class RegistrySnapshotController {
             @RequestPart("file") MultipartFile snapshot){
 
         PdfDownloadResponse result = registryWorkflowService.executePostContractDiagnosis(houseId, snapshot);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/houses/pre-contract-diganoses")
+    public ResponseEntity<PdfDownloadResponse> diagnosePreContract(
+            @RequestPart("file") MultipartFile snapshot,
+            @RequestPart("data")PreContractDiagnosisRequest request){
+
+        PdfDownloadResponse result = registryWorkflowService.executePreContractDiagnosis(request, snapshot);
         return ResponseEntity.ok(result);
     }
 }
