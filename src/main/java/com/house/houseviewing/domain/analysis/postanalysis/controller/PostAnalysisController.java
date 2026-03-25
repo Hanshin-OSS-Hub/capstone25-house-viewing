9eda0fd8fa31962bf206154e737cc721bdd3b18e
Yoo <yooyoo9191@gmail.com>
2026-03-25 16:06:44 +0900

    fix: typo

--- src/main/java/com/house/houseviewing/domain/analysis/postanalysis/controller/PostAnalysisController.java
+++ src/main/java/com/house/houseviewing/domain/analysis/postanalysis/controller/PostAnalysisController.java
@@ -16,7 +16,7 @@ public class PostAnalysisController {
 
     private final DiffAnalysisQueryService diffAnalysisQueryService;
 
-    @PostMapping("/{houseId}}/change-diagnoses")
+    @PostMapping("/{houseId}/change-diagnoses")
     public ResponseEntity<PdfDownloadResponse> diffDiagnose(@PathVariable Long houseId){
         PdfDownloadResponse result = diffAnalysisQueryService.executeDiffDiagnosis(houseId);
         return ResponseEntity.ok(result);
