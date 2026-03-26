ead3142b3fb7ceb9aa6e8a00ce9a22f3780d32b2
Yoo <yooyoo9191@gmail.com>
2026-03-26 17:18:17 +0900

    feat: RefreshTokenService

--- src/main/java/com/house/houseviewing/domain/auth/service/RefreshTokenService.java
+++ src/main/java/com/house/houseviewing/domain/auth/service/RefreshTokenService.java
@@ -1,9 +1,32 @@
 package com.house.houseviewing.domain.auth.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
 @Service
+@RequiredArgsConstructor
 public class RefreshTokenService {
 
-    public 
+    public static final String REFRESH_PREFIX = "refresh:";
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public void saveRefreshToken(Long userId, String refreshToken, long expirationMs){
+        stringRedisTemplate.opsForValue().set(generateKey(userId), refreshToken, Duration.ofMillis(expirationMs));
+    }
+
+    public String getRefreshToken(Long userId){
+        return stringRedisTemplate.opsForValue().get(generateKey(userId));
+    }
+
+    public void deleteRefreshToken(Long userId){
+        stringRedisTemplate.delete(generateKey(userId));
+    }
+
+    private String generateKey(Long userId){
+        return REFRESH_PREFIX + userId;
+    }
 }
