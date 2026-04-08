2ff2e1aa5deeed0bd97dd9a263d4b2c9fc0f4e5f
mok <dnjswn0516@gmail.com>
2026-04-08 15:56:41 +0900

    feat: HouseDetailItem에 문서 URI 필드 추가

--- app/src/main/java/com/capstone/houseviewingapp/data/local/model/HouseDetailItem.kt
+++ app/src/main/java/com/capstone/houseviewingapp/data/local/model/HouseDetailItem.kt
@@ -5,6 +5,7 @@ data class HouseDetailItem(
     val homeName: String,
     val originAddress: String,
     val detailAddress: String,
+    val documentUri: String? = null,
     val contractType: String, // "JEONSE" | "WOLSE"
     val deposit: Long,
     val monthlyAmount: Long,
