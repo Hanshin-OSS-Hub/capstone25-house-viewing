f04e31496eca4dd89bd804f2cedcfeffddbf501b
mok <dnjswn0516@gmail.com>
2026-04-25 23:57:07 +0900

    refactor: 구독 저장소 제공자를 원격 구현 기반으로 전환한다

--- app/app/src/main/java/com/capstone/houseviewingapp/subscription/SubscriptionRepositoryProvider.kt
+++ app/app/src/main/java/com/capstone/houseviewingapp/subscription/SubscriptionRepositoryProvider.kt
@@ -1,6 +1,6 @@
 package com.capstone.houseviewingapp.subscription
 
 object SubscriptionRepositoryProvider {
-    val repository: SubscriptionRepository by lazy { MockSubscriptionRepository() }
+    val repository: SubscriptionRepository by lazy { RemoteSubscriptionRepository() }
 }
 
