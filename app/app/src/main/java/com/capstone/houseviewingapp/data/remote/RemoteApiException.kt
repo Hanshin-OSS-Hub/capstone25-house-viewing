3928419c8bcb5fddb8cb40903f08b310bcf1628f
mok <dnjswn0516@gmail.com>
2026-04-25 23:54:47 +0900

    refactor: 원격 예외 모델 필드를 오류 처리 흐름에 맞게 정리한다

--- app/app/src/main/java/com/capstone/houseviewingapp/data/remote/RemoteApiException.kt
+++ app/app/src/main/java/com/capstone/houseviewingapp/data/remote/RemoteApiException.kt
@@ -2,5 +2,7 @@ package com.capstone.houseviewingapp.data.remote
 
 class RemoteApiException(
     val code: String?,
+    val statusCode: Int?,
+    val rawBody: String?,
     override val message: String
 ) : Exception(message)
