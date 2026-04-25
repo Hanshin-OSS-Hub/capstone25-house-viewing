a9d209e2b069f4f7a5c68660d25616ad5f0a5d79
mok <dnjswn0516@gmail.com>
2026-04-25 23:53:17 +0900

    refactor: 인증 저장소 제공자를 원격 구현 중심으로 단순화한다

--- app/app/src/main/java/com/capstone/houseviewingapp/auth/AuthRepositoryProvider.kt
+++ app/app/src/main/java/com/capstone/houseviewingapp/auth/AuthRepositoryProvider.kt
@@ -1,16 +1,5 @@
 package com.capstone.houseviewingapp.auth
 
-import com.capstone.houseviewingapp.BuildConfig
-
-/**
- * [BuildConfig.USE_MOCK_API] 가 true 이면 Mock, false 이면 Retrofit + Bearer JWT
- */
 object AuthRepositoryProvider {
-    val repository: AuthRepository by lazy {
-        if (BuildConfig.USE_MOCK_API) {
-            MockAuthRepository()
-        } else {
-            RemoteAuthRepository()
-        }
-    }
+    val repository: AuthRepository by lazy { RemoteAuthRepository() }
 }
