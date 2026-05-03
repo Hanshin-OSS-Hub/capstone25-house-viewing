afb51d67c16ef07e4ca5f72bf9ac8e66338624e8
mok <dnjswn0516@gmail.com>
2026-05-03 22:56:24 +0900

    feat: houses 단건/목록 조회 API 추가로 LTV 조회 경로 확장

--- app/app/src/main/java/com/capstone/houseviewingapp/data/remote/api/HouseApi.kt
+++ app/app/src/main/java/com/capstone/houseviewingapp/data/remote/api/HouseApi.kt
@@ -1,11 +1,15 @@
 package com.capstone.houseviewingapp.data.remote.api
 
+import com.capstone.houseviewingapp.house.model.HouseMeResponse
+import com.capstone.houseviewingapp.house.model.HousesResponse
 import com.capstone.houseviewingapp.registration.remote.HouseRegisterRequest
 import com.capstone.houseviewingapp.registration.remote.HouseRegisterResponse
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface HouseApi {
     @POST("houses/register")
@@ -13,4 +17,15 @@ interface HouseApi {
         @Header("Authorization") authorization: String,
         @Body body: HouseRegisterRequest
     ): Call<HouseRegisterResponse>
+
+    @GET("houses/{houseId}")
+    fun getHouse(
+        @Header("Authorization") authorization: String,
+        @Path("houseId") houseId: Long
+    ): Call<HouseMeResponse>
+
+    @GET("houses")
+    fun getHouses(
+        @Header("Authorization") authorization: String
+    ): Call<List<HousesResponse>>
 }
